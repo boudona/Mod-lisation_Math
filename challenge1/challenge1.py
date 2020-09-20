29dc777a0a94883228a96ac272c7631c40b7b034
daniel-rajaonarivelo <daniel.rajaonarivelo@etu.umontpellier.fr>
2020-09-20 21:20:38 +0200

    ajout commentaire + correction images

--- challenge1/challenge1.py
+++ challenge1/challenge1.py
@@ -11,22 +11,23 @@ cheval1 = plt.imread('chevaux.png', 'PNG') # import dans un tableau numpy
 cheval2 = plt.imread('chevaux.png', 'PNG')
 
 
-chevaux1 = np.concatenate((cheval1,cheval2),axis=0)
+chevaux1 = np.concatenate((cheval1,cheval2),axis=0) # concatenation à droite
+#equivalent à: "chevaux1 = np.block([[cheval1, cheval2]])"
 
 plt.imshow(chevaux1)
 plt.show()
-plt.imsave('doubleHorizontal.png', chevaux1)
+plt.imsave('doubleVertical.png', chevaux1)
 
-chevaux2 = np.concatenate((cheval1,cheval2),axis=1)
+chevaux2 = np.concatenate((cheval1,cheval2),axis=1) #concatenation à gauche
 plt.imshow(chevaux2)
 plt.show()
-plt.imsave('doubleVertical.png', chevaux2)
+plt.imsave('doubleHorizontal.png', chevaux2)
 
-chevaux21 = plt.imread('doubleVertical.png', 'PNG') # import dans un tableau numpy
-chevaux22 = plt.imread('doubleVertical.png', 'PNG')
+chevaux21 = plt.imread('doubleHorizontal.png', 'PNG') # import dans un tableau numpy
+chevaux22 = plt.imread('doubleHorizontal.png', 'PNG')
 
 chevaux3 = np.concatenate((chevaux21,chevaux22),axis=0)
 plt.imshow(chevaux3)
 plt.show()
-plt.imsave('quadruple.png', chevaux2)
+plt.imsave('quadruple.png', chevaux3)
 
