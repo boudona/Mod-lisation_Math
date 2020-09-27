9c139ab9352272bd2b5df77519e300289fb01b4d
boudona <arthurcrouton@OMG.localdomain>
2020-09-27 13:25:32 +0200

    correction exo2

--- challenge2/challenge2.py
+++ challenge2/challenge2.py
@@ -11,35 +11,23 @@ plt.show()
 
 
 
-chevaux1 = cheval1[:,::-1,:]
+chevaux1 = cheval1[::-1,:,:]
 
 
 plt.imshow(chevaux1)
 plt.show()
-plt.imsave('simpleflip.png', chevaux1)
 
 
 
-#cheval2 = plt.imread('doubleVertical.png', 'PNG') # import dans un tableau numpy
 
-#chevaux2 = np.flip((cheval2),axis=0)
+chevauxdouble = np.concatenate((cheval1,cheval1),axis=1)
+chevauxdoubleinverse = chevauxdouble[::-1,:,:]
 
-
-#plt.imshow(chevaux2)
-#plt.show()
-#plt.imsave('doubleflip.png', chevaux2)
+chevaux2=np.concatenate((chevauxdouble,chevauxdoubleinverse),axis=0)
 
 
-#cheval3 = plt.imread('doubleflip.png', 'PNG') # import dans un tableau numpy
+plt.imshow(chevaux2)
+plt.show()
 
-#chevaux3 = np.concatenate((cheval2,cheval3),axis=0)
-
-
-#plt.imshow(chevaux3)
-#plt.show()
-#plt.imsave('doubleflip.png', chevaux3)
-
-
-#plt.imsave('retourner.png', chevaux1)
 
 
